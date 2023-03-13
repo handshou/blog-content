8b4f73d10589cb6cd7a8ba0a8a6256a62bf79724
handshou <hanselhub@gmail.com>
2023-03-13 20:46:30 +0800

    fix: save to blog folder

--- queryPage.js
+++ queryPage.js
@@ -3,9 +3,14 @@ import fs from 'fs'
 let queryPage = async (n2m, pageId, pageName) => {
   const mdblocks = await n2m.pageToMarkdown(pageId);
   const mdString = n2m.toMarkdownString(mdblocks);
+  const dir = './blog';
 
+  //writing to folder
+  if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+  }
   //writing to file
-  fs.writeFile(pageName, mdString, (err) => {
+  fs.writeFile(`./${dir}/${pageName}`, mdString, (err) => {
     if (err)
         console.log(err);
   });
